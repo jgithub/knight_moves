5865e5f6ab7771dc8790c0ec3096025fbbc05afb
jgithub <jgithub1@jeano.net>
2021-12-28 08:52:17 -0600

    separate cli.ts

--- src/knight_moves.ts
+++ src/knight_moves.ts
@@ -57,7 +57,7 @@ function trace (msg: any, ...args: any[]): void {
   // }
 }
 
-function debug (msg: any, ...args: any[]): void {
+export function debug (msg: any, ...args: any[]): void {
   // if ( args.length > 0 ) {
   //   console.log(msg, args)
   // } else {
@@ -114,7 +114,7 @@ const iterateAllDirections: KnightMoveDirection[] = Object.freeze([
  * This represents a single square on a chess board
  * 1,1 is the lower left-hand corner of the board
  */
-class ChessBoardSquare {
+export class ChessBoardSquare {
   private readonly m_x: number
   private readonly m_y: number
 
@@ -155,7 +155,7 @@ class ChessBoardSquare {
   }
 }
 
-class ChessBoard {
+export class ChessBoard {
   private readonly m_numCols: number
   private readonly m_numRows: number
 
@@ -176,7 +176,7 @@ class ChessBoard {
   }
 }
 
-class KnightInTransit {
+export class KnightInTransit {
   private readonly m_currentSquare: ChessBoardSquare
   private readonly m_destinationSquare: ChessBoardSquare
 
@@ -282,7 +282,7 @@ class KnightInTransit {
   }
 }
 
-class KnightInTransitMovementHistory {
+export class KnightInTransitMovementHistory {
   private readonly m_history: KnightInTransit[]
 
   constructor (existingHistory: KnightInTransit[] | undefined) {
@@ -502,60 +502,7 @@ function determineBestHistoryThatWorks (knightInTransitMovementHistory: KnightIn
   return bestHistorySoFar
 }
 
-let chessBoard: ChessBoard
-let knightStartingSquare: ChessBoardSquare
-let knightDestinationSquare: ChessBoardSquare
-
-/*
-       d8888                              
-      d88888                              
-     d88P888                              
-    d88P 888   888d888 .d88b.    888  888 
-   d88P  888   888P"  d88P"88b   888  888 
-  d88P   888   888    888  888   Y88  88P 
- d8888888888   888    Y88b 888    Y8bd8P  
-d88P     888   888     "Y88888     Y88P   
-                           888            
-                      Y8b d88P            
-                       "Y88P"             
-*/
-{
-  const myArgs = process.argv.slice(2)
-  debug(`myArgs = ${JSON.stringify(myArgs)}`)
-
-  const keyValuePairs: Map<string, string> = new Map()
-
-  for (let ii = 0; ii < myArgs.length; ii++) {
-    const key = myArgs[ii]
-    ii++
-    const value = myArgs[ii]
-    debug(`Handling key/value = '${key}'/'${value}'`)
-    keyValuePairs.set(key, value)
-  }
-
-
-  /* Handle Argv */
-  const argKeys = Array.from( keyValuePairs.keys() );
-  for (let ii = 0; ii < argKeys.length; ii++) {
-    const key = argKeys[ii]
-    const value = keyValuePairs.get(key)
-    debug(`Handling key/value = '${key}'/'${value}'`)
-
-    if (key === '--board_size') {
-      const [x, y] = (value as string).split(',')
-      debug(`Handling board_size = '${value}'`)
-      chessBoard = new ChessBoard(Number(x), Number(y))
-    } else if (key === '--source') {
-      const [x, y] = (value as string).split(',')
-      knightStartingSquare = new ChessBoardSquare(Number(x), Number(y))
-    } else if (key === '--dest') {
-      const [x, y] = (value as string).split(',')
-      knightDestinationSquare = new ChessBoardSquare(Number(x), Number(y))
-    }
-  }
-}
-
-class KnightMoveRunner {
+export class KnightMoveRunner {
   public run(chessBoard: ChessBoard, knightStartingSquare: ChessBoardSquare, knightDestinationSquare: ChessBoardSquare): KnightInTransitMovementHistory | undefined {
     const knightInTransit: KnightInTransit = KnightInTransit.createWithStartingSquareAndDestinationInMind(chessBoard, knightStartingSquare, knightDestinationSquare)
     let knightInTransitMovementHistory: KnightInTransitMovementHistory | undefined = new KnightInTransitMovementHistory([knightInTransit])
@@ -564,20 +511,3 @@ class KnightMoveRunner {
     return knightInTransitMovementHistory
   }
 }
-
-const knightMoveRunner: KnightMoveRunner = new KnightMoveRunner()
-const knightInTransitMovementHistory: KnightInTransitMovementHistory | undefined = knightMoveRunner.run(chessBoard, knightStartingSquare, knightDestinationSquare)
-if (knightInTransitMovementHistory != null && knightInTransitMovementHistory.getSize() > 0) {
-  console.log(`>> ${knightInTransitMovementHistory.toString()}`)
-  console.log(`>> ${knightInTransitMovementHistory.getNumDifferentSquares()}`)
-} else {
-  console.log('-1')
-}
-
-// % npx ts-node knight_moves.ts --board_size 11,11 --dest 11,11 --source 1,1
-// % npx ts-node knight_moves.ts --board_size 11,11 --dest 10,10 --source 1,1
-// % ./knight_moves --board_size 3,3 --dest 3,3 --source 1,1
-// % ./knight_moves --board_size 27,27 --dest 27,27 --source 1,1
-// >> (1, 1) -> (3, 2) -> (5, 3) -> (7, 4) -> (9, 5) -> (11, 6) -> (13, 7) -> (15, 8) -> (17, 9) -> (19, 10) -> (21, 11) -> (22, 13) -> (23, 15) -> (24, 17) -> (25, 19) -> (26, 21) -> (27, 23) -> (26, 25) -> (27, 27)
-// >> 19
-// jeano@MacBook-Pro app %
